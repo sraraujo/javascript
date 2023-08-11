49e6fe009db936a300591631dce01c25c7be6b50
Jonas Araújo <135404598+sraraujo@users.noreply.github.com>
2023-08-11 02:52:17 -0300

    testes da aula 15

---
+++ aula16/ex001.js
@@ -0,0 +1,14 @@
+/*
+var lista = [5, 8, 2, 9, 3]
+
+lista.sort()
+lista.push(1)
+
+for (var numero in lista){
+    numero = Number(numero)
+    console.log(`${numero +1}° - ${lista[numero]}`)
+}
+console.log(`O vaetor possui ${lista.length} elemento(s)`)
+console.log(`O primeiro elemento é ${lista[0]}`)
+*/
+
---
+++ aula16/funcao04.js
@@ -0,0 +1,10 @@
+function fatorial(x){
+    var fat = 1
+
+    for (var c = x; c > 1; c--){
+        fat *= c
+    }
+    return(fat)
+}
+
+console.log(fatorial(5))
